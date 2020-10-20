054eca180620eaeb02c42ae3811caf6c505634e5
Angel <acalderon@activeit.cl>
2020-10-20 09:38:12 -0300

    se corrige spinner

--- src/components/UI/Spinner.tsx
+++ src/components/UI/Spinner.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import './Spinner.css';
+import React from "react";
+import "./Spinner.css";
 
 export const Spinner = () => {
   return (
-    <div className='absolute w-full h-full' style={{ top: '30%', left: '50%' }}>
-      <div className='loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-64 w-64'></div>
-      {/* <div className='loader ease-linear rounded-full border-8 border-t-8 border-green-200 h-24 w-24'></div> */}
+    <div className="flex w-full h-full justify-center">
+      <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-20 w-20"></div>
     </div>
   );
 };
