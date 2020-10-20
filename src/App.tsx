e2b0bf498a16eb8346f635639074e5225dd2b335
Angel <acalderon@activeit.cl>
2020-10-19 22:50:37 -0300

    pages

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './App.css';
-import Portfolio from './components/portfolio/Portfolio';
-
-function App() {
-  return <Portfolio></Portfolio>;
-}
-
-export default App;
