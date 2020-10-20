34dff9375706c6e5ebb3f1b02239ee3ca3e66ec3
Angel <acalderon@activeit.cl>
2020-10-20 19:58:08 -0300

    se ajusta spinner

--- src/components/UI/Spinner.tsx
+++ src/components/UI/Spinner.tsx
@@ -3,7 +3,7 @@ import "./Spinner.css";
 
 export const Spinner = () => {
   return (
-    <div className="flex w-full h-full justify-center">
+    <div className="flex m-0 w-full h-full justify-center">
       <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-20 w-20"></div>
     </div>
   );
