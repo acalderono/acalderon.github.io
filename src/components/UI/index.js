3d5a359a2e928ff199c0a4c80b287b3768cee7f0
Angel <acalderon@activeit.cl>
2021-06-12 17:45:32 -0400

    index

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -2,5 +2,5 @@ export { Header } from "./organisms/Header/Header";
 export { Me } from "./organisms/Me/Me";
 export { AboutMe } from "./organisms/AboutMe/AboutMe";
 export { LastPosts } from "./organisms/LastPosts/LastPosts";
-export { Projects } from "./organisms/Projects";
+export { Projects } from "./organisms/Projects/Projects";
 export { Footer } from "./organisms/Footer";
--- src/components/UI/molecules/index.js
+++ src/components/UI/molecules/index.js
@@ -1,2 +1,3 @@
 export { SeeMore } from "./SeeMore/SeeMore.jsx";
 export { Post } from "./Post/Post.jsx";
+export { Project } from "./Project/Project.jsx";
