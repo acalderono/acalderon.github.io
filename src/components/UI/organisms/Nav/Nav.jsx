99a18becc8076475118aec6825de5d978a62fb10
Angel <acalderon@activeit.cl>
2021-06-14 15:06:13 -0400

    add icon

--- src/components/UI/organisms/Nav/Nav.jsx
+++ src/components/UI/organisms/Nav/Nav.jsx
@@ -1,24 +1,61 @@
-import { Container, Link } from "./Styles";
+import { Container, Link, Icon } from "./Styles";
 
-export const Nav = (username = "acalderono", isDark = true) => {
+export const Nav = ({ username = "acalderono", isDark = true }) => {
   const social = [
     {
-      name: `https://twitter.com/${username}`,
+      href: `https://twitter.com/${username}`,
+      name: "twitter",
+      rel: "noopener noreferrer",
+    },
+    {
+      href: `https://github.com/${username}`,
+      name: "github",
+      rel: "noopener noreferrer",
+    },
+    {
+      href: `https://linkedin.com/in/${username}`,
+      name: "linkedin",
+      rel: "noopener noreferrer",
+    },
+    {
+      href: `mailto:angel@acalderon.dev`,
+      name: "envelope",
+      rel: "",
+    },
+  ];
+
+  return (
+    <Container>
+      {social?.map(({ href, rel, name }, i) => {
+        return (
+          <Link isDark key={i} href={href} rel={rel}>
+            <Icon nameIcon={name}></Icon>
+          </Link>
+        );
+      })}
+    </Container>
+  );
+};
+
+export const Nav2 = ({ username = "acalderono", isDark = true }) => {
+  const social = [
+    {
+      href: `https://twitter.com/${username}`,
       icon: "fab fa-twitter",
       rel: "noopener noreferrer",
     },
     {
-      name: `https://github.com/${username}`,
+      href: `https://github.com/${username}`,
       icon: "fab fa-github",
       rel: "noopener noreferrer",
     },
     {
-      name: `https://linkedin.com/in/${username}`,
+      href: `https://linkedin.com/in/${username}`,
       icon: "fab fa-linkedin",
       rel: "noopener noreferrer",
     },
     {
-      name: `mailto:angel@acalderon.dev"}`,
+      href: `mailto:angel@acalderon.dev`,
       icon: "far fa-envelope",
       rel: "",
     },
@@ -26,10 +63,10 @@ export const Nav = (username = "acalderono", isDark = true) => {
 
   return (
     <Container className="icons-social">
-      {social?.map((rs, i) => {
+      {social?.map(({ href, rel, icon }, i) => {
         return (
-          <Link key={i} rel={rs.rel} href={rs.name}>
-            <i className={rs.icon}></i>
+          <Link isDark key={i} href={href} rel={rel}>
+            <i className={icon}></i>
           </Link>
         );
       })}
